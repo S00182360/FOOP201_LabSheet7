20e775b2903b9904aaafd4053c224847e072bd03
Samuel Colville <S00182360@mail.itsligo.ie>
2018-11-21 14:37:18 +0000

    Q3

--- FOOP201_LabSheet7/Program.cs
+++ FOOP201_LabSheet7/Program.cs
@@ -10,6 +10,14 @@ namespace FOOP201_LabSheet7
     {
         static void Main(string[] args)
         {
+            Game g1 = new Game("Monopoly", 19.99m, new DateTime(1970, 01, 31));
+
+            Game g2 = new Game() { Price = 10.99m, ReleaseDate = new DateTime(2000, 6, 15) };
+
+            Console.WriteLine(g1);
+            Console.WriteLine(g2);
+
+            Console.ReadKey();
 
         }
     }
@@ -25,24 +33,24 @@ namespace FOOP201_LabSheet7
 
         public Game(string name, decimal price) : this(name, price, DateTime.Now)
         {
-
+            this.name = name;
+            Price = price;
         }
 
-        public Game() : this("", 0)
+        public Game() : this("DEFAULT NAME", 0)
         {
 
         }
 
-
         readonly string name;
         public string Name { get { return name; } }
-        public decimal Price { get; }
-        public DateTime ReleaseDate { get; }
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
 
 
         public override string ToString()
         {
-            return String.Format("Name: {0}/nPrice: {1}EUR/nRelease Date: {2}/n/n", Name, Price, ReleaseDate);
+            return String.Format("Name: {0}\nPrice: {1}EUR\nRelease Date: {2}\n\n", Name, Price, ReleaseDate);
         }
     }
 }
