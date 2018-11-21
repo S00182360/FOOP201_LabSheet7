9f43b5b5356c1c3825859c826d02ca94f7e6a47a
Samuel Colville <S00182360@mail.itsligo.ie>
2018-11-21 14:01:41 +0000

    Q1 and Q2 complete

--- FOOP201_LabSheet7/Program.cs
+++ FOOP201_LabSheet7/Program.cs
@@ -10,6 +10,39 @@ namespace FOOP201_LabSheet7
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    class Game
+    {
+        public Game(string name, decimal price, DateTime releaseDate)
+        {
+            this.name = name;
+            Price = price;
+            ReleaseDate = releaseDate;
+        }
+
+        public Game(string name, decimal price) : this(name, price, DateTime.Now)
+        {
+
+        }
+
+        public Game() : this("", 0)
+        {
+
+        }
+
+
+        readonly string name;
+        public string Name { get { return name; } }
+        public decimal Price { get; }
+        public DateTime ReleaseDate { get; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}/nPrice: {1}EUR/nRelease Date: {2}/n/n", Name, Price, ReleaseDate);
         }
     }
 }
