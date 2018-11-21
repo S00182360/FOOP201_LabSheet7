7c839938ce7bd2cf2484bdca5cdcb327e156d0c1
Samuel Colville <S00182360@mail.itsligo.ie>
2018-11-21 15:13:11 +0000

    Q4 and Q5

--- FOOP201_LabSheet7/Program.cs
+++ FOOP201_LabSheet7/Program.cs
@@ -12,13 +12,25 @@ namespace FOOP201_LabSheet7
         {
             Game g1 = new Game("Monopoly", 19.99m, new DateTime(1970, 01, 31));
 
-            Game g2 = new Game() { Price = 10.99m, ReleaseDate = new DateTime(2000, 6, 15) };
+            //Game g2 = new Game() { Price = 10.99m, ReleaseDate = new DateTime(2000, 6, 15) };
 
-            Console.WriteLine(g1);
-            Console.WriteLine(g2);
+            ComputerGame cg1 = new ComputerGame("Tomb Raider", 49.99m, new DateTime(1996, 10, 25), "15+");
+            ComputerGame cg2 = new ComputerGame("Legend of Zelda", 5.99m, new DateTime(1986, 02, 21), "3+");
+
+            DisplayGame(g1);
+            Console.WriteLine("");
+            //Console.WriteLine(g2);
+            DisplayGame(cg1);
+            Console.WriteLine("");
+            DisplayGame(cg2);
 
             Console.ReadKey();
 
+        }
+
+        static void DisplayGame(Game g)
+        {
+            Console.WriteLine(g);
         }
     }
 
@@ -44,13 +56,37 @@ namespace FOOP201_LabSheet7
 
         readonly string name;
         public string Name { get { return name; } }
-        public decimal Price { get; set; }
+        protected decimal Price { get; set; }
         public DateTime ReleaseDate { get; set; }
 
 
         public override string ToString()
         {
-            return String.Format("Name: {0}\nPrice: {1}EUR\nRelease Date: {2}\n\n", Name, Price, ReleaseDate);
+            return String.Format("Name: {0}\nPrice: {1}EUR\nRelease Date: {2}\n", Name, Price, ReleaseDate);
+        }
+    }
+
+    class ComputerGame : Game
+    {
+        public ComputerGame(string name, decimal price, DateTime releaseDate, string pEGIRating) : base(name, price, releaseDate)
+        {
+            PEGIRating = pEGIRating;
+        }
+        public ComputerGame()
+        {
+
+        }
+
+        public string PEGIRating { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + String.Format("PEGI: {0}\n", PEGIRating);
+        }
+
+        public decimal GetDiscountPrice()
+        {
+            return Price * 0.9m;
         }
     }
 }
